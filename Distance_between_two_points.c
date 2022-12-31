6c06cf15f74c77ab36a4f758c528a6d592248922
umachittudi2004 <119730219+umachittudi2004@users.noreply.github.com>
2022-12-31 15:09:03 +0530

    #include<stdio.h> #include<math.h> void uma(int x1,int x2,int y1,int y2) {     float d;     d=sqrt((x2-x1)*(x2-x1)+(y2-y1)*(y2-y1));     printf("%.4f",d); } int main() {     int x1,x2,y1,y2;     scanf("%d%d%d%d",&x1,&y1,&x2,&y2);     uma(x1,x2,y1,y2); }

---
+++ Distance_between_two_points.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<math.h>
+void uma(int x1,int x2,int y1,int y2)
+{
+    float d;
+    d=sqrt((x2-x1)*(x2-x1)+(y2-y1)*(y2-y1));
+    printf("%.4f",d);
+}
+int main()
+{
+    int x1,x2,y1,y2;
+    scanf("%d%d%d%d",&x1,&y1,&x2,&y2);
+    uma(x1,x2,y1,y2);
+}
