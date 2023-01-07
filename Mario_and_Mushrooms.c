f9b2ccac1eb01432d901cfd75882c385df2af6df
umachittudi2004 <119730219+umachittudi2004@users.noreply.github.com>
2023-01-07 22:11:39 +0530

    #include<stdio.h> int main() {     int a;     scanf("%d",&a);     if(a%3==0)     {         printf("NORMAL ");     }     else if     {         (a%3==1) printf("HUGE ");     }     else     {         printf("SMALL ");     } }

---
+++ Mario_and_Mushrooms.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main()
+{
+    int a;
+    scanf("%d",&a);
+    if(a%3==0) 
+    {
+        printf("NORMAL
+");
+    }
+    else if
+    {
+        (a%3==1) printf("HUGE
+");
+    }
+    else 
+    {
+        printf("SMALL
+");
+    }
+}
