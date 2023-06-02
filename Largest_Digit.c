843368a55e425b59c3903e5a8d65d7607a4c3458
umachittudi2004 <119730219+umachittudi2004@users.noreply.github.com>
2023-06-02 21:24:41 +0530

    #include<stdio.h>
    int main() 
    
    { 
    int n,max=0,c=0; 
    scanf("%d",&n); 
    int i=0; 
    int a[100]; 
    while(n!=0) 
    { 
    int r=n%10; 
    a[i]=r; 
    i++; 
    c++; 
    n=n/10; 
    } 
    for(int i=0;i<c;i++) 
    { 
    if(a[i]>max) 
    {
        max=a[i]; 
    } 
    
    } 
    printf("%d",max); 
    }

--- Largest_Digit.c
+++ Largest_Digit.c
@@ -1,16 +1,29 @@
-#include<stdio.h>
-int main()
+#include<stdio.h> 
+
+int main() 
+
+{ 
+int n,max=0,c=0; 
+scanf("%d",&n); 
+int i=0; 
+int a[100]; 
+while(n!=0) 
+{ 
+int r=n%10; 
+a[i]=r; 
+i++; 
+c++; 
+n=n/10; 
+} 
+for(int i=0;i<c;i++) 
+{ 
+if(a[i]>max) 
 {
-    int n,r,max=0;
-    scanf("%d",&n);
-    while(n!=0)
-    {
-        r=n%10;
-        if(r>max)
-        {
-            max=r;
-        }
-        n=n/10;
-    }
-    printf("%d",max);
-}
+    max=a[i]; 
+} 
+
+} 
+printf("%d",max); 
+} 
+
+    
