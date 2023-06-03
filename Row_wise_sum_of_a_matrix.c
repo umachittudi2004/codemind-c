7377c6eeb8efa40ff8e9eb3b1887dfe6a9987bcd
umachittudi2004 <119730219+umachittudi2004@users.noreply.github.com>
2023-06-03 07:56:30 +0530

    #include<stdio.h>
    int main() 
    
    { 
    int r,c; 
    scanf("%d%d",&r,&c); 
    int mat[r][c]; 
    for(int i=0;i<r;i++) 
    { 
    for(int j=0;j<c;j++) 
    { 
    scanf("%d",&mat[i][j]); 
    } 
    } 
    for(int i=0;i<r;i++) 
    { 
    int s=0; 
    for(int j=0;j<c;j++) 
    { 
    s=s+mat[i][j]; 
    } 
    printf("%d ",s); 
    } 
    }

---
+++ Row_wise_sum_of_a_matrix.c
@@ -0,0 +1,25 @@
+#include<stdio.h> 
+
+int main() 
+
+{ 
+int r,c; 
+scanf("%d%d",&r,&c); 
+int mat[r][c]; 
+for(int i=0;i<r;i++) 
+{ 
+for(int j=0;j<c;j++) 
+{ 
+scanf("%d",&mat[i][j]); 
+} 
+} 
+for(int i=0;i<r;i++) 
+{ 
+int s=0; 
+for(int j=0;j<c;j++) 
+{ 
+s=s+mat[i][j]; 
+} 
+printf("%d ",s); 
+} 
+}
